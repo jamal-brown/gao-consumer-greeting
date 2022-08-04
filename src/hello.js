5aa721f2f933b76f94240d4956da4b0c35b6f523
jamal-brown <90861761+jamal-brown@users.noreply.github.com>
2022-08-04 09:12:53 -0400

    Create hello.js

---
+++ src/hello.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "https://jar-spring-rest-service.azurewebsites.net/greeting"
+    }).then(function(data, status, jqxhr) {
+       $('.greeting-id').append(data.id);
+       $('.greeting-content').append(data.content);
+       console.log(jqxhr);
+    });
